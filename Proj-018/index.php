13064c6d735f50820374b7d8b48273a6cb6c71e5
samucarsilva <samucarsilva007@gmail.com>
2025-02-13 11:17:02 -0300

    Desafios PHP v1.0.1

--- Proj-018/index.php
+++ Proj-018/index.php
@@ -6,7 +6,7 @@
     <title>Contando Números</title>
 </head>
 <body>
-    
+
 
     <h2> Contando Números </h2>
 
@@ -14,12 +14,23 @@
     <?php 
 
 
-        $listaDeNumeros = [2, 67, 98, 100, 558, 620, 899, 985, 996, 80, 256, 88];
-    
+        $quebraDeLinha = "<br></br>";
+
+
+        $listaDeNumeros = [15, 8, 5, 80, 33, 620, 25, 13, 75, 80, 256, 88];
+
 
         $numerosPares = array_filter($listaDeNumeros, fn($value) => $value % 2 === 0);
+        $quantidadeTotal = count($numerosPares);
 
-        print_r($numerosPares);
+
+            print_r($numerosPares);
+
+
+        echo $quebraDeLinha;
+
+
+        echo "A quantidade total de números pares equivale a: ", $quantidadeTotal;
 
 
     ?>
